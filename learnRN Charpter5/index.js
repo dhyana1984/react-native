1b86eca58110c675fed545f4f6343e0458ded513
dhyana1984 <xiongyi_1984@foxmail.com>
2019-08-26 00:49:03 +0800

    flex布局和View属性

--- learnRN Charpter5/index.js
+++ learnRN Charpter5/index.js
@@ -2,7 +2,7 @@
  * @format
  */
 import React,{Component} from 'react';
-import {AppRegistry,StyleSheet,View} from 'react-native';
+import {AppRegistry,StyleSheet,View,Text} from 'react-native';
 
 
 
@@ -11,23 +11,39 @@ export default class LearnRN extends Component{
     render(){
         return (
             <View style={styles.container}> 
-                <View style={styles.firstRow}>
-                    <View style={styles.test1}>
-
-                    </View>
-                    <View style={styles.test2}>
-
-                    </View>
-                    <View style={styles.test3}>
-
-                    </View>
-                    <View style={styles.test3}>
-
-                    </View>
-                    <View style={styles.test3}>
-
-                    </View>
-                </View> 
+                <Text style={styles.welcome0}>
+                    aaWelcome to React Native!
+                </Text>
+                <Text style={styles.welcome1}>
+                    aaWelcome to React Native!
+                </Text>
+                {/* <Text style={styles.welcome2}>
+                    aaWelcome to React Native!
+                </Text>
+                <Text style={styles.welcome3}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.welcome4}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.welcome5}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.welcome6}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.welcome7}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.welcome8}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.welcome9}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.welcome10}>
+                    Welcome to React Native!
+                </Text> */}
             </View>
         )
     }
@@ -36,16 +52,101 @@ export default class LearnRN extends Component{
 
 const styles = StyleSheet.create({
     container:{
-        flex : 1,
-        backgroundColor:'white',
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#F5FCFF',
+        
+    },
+    welcome0:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        
+    },
+    welcome1:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{skewX:'90deg'}]
+    },
+    welcome2:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{rotateY:'90deg'}]
+    },
+    welcome3:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{rotateZ:'45deg'}]
+    },
+    welcome4:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{scale:2}]
+    },
+    welcome5:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{scaleX:2}]
+    },
+
+    welcome6:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{scaleY:2}]
+    },
+    welcome7:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{translateX:200}]
+    },
+    welcome8:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{translateY:150}]
+    },
+    welcome9:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{skewX:'45deg'}]
+    },
+    welcome10:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        transform:[{skewY:'45deg'}]
+    },
+    vs:{
+        height:50,
+        backgroundColor:'gray',
+    },  
+    vs1:{
+        flex:1,
+        height:50,
+        backgroundColor:'black',
+    },
+    vs2:{
+        flex:2,
+        height:50,
+        backgroundColor:'black',
     },
     firstRow:{
-        height: 40,
+        height: 80,
         top :40,
         backgroundColor:'black',
         flexDirection:'row',
-        //justifyContent:'center',
-        alignItems:'center'
+        
+        alignItems:'stretch',
+        
     },
     test1:{
         width: 68,
@@ -62,13 +163,21 @@ const styles = StyleSheet.create({
         height:24,
         backgroundColor:'white',
     },
+    textStyle:{
+        fontSize:18,
+        color:"white",
+        backgroundColor:"gray",
+    },
     testPosition:{
         backgroundColor:'gray',
-        height:60,
-        width:60,
+        height:200,
+        width:100,
         position:'absolute',
-        top:150,
-        right:50
+        top:350,
+        right:150,
+        
+        
+        
     }
 });
 
